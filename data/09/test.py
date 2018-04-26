afd3e54b6bc559c75132e9a6a1ff68564ccc044c
mtrempoltsev <mt@devexp.ru>
2018-04-26 06:57:49 +0300

    homework 09

---
+++ data/09/test.py
@@ -0,0 +1,30 @@
+def isPing(line):
+    line = line.rstrip()
+    if line == "ping":
+        return True
+    if line == "pong":
+        return False
+    print("not ping not pong")
+    quit()
+
+n = 0
+with open("pingpong.txt") as file:
+    line = file.readline()
+    if not line:
+        print("empty output")
+        quit()
+
+    last = isPing(line)
+    for line in file:
+        current = isPing(line)
+        if last == current:
+            print("failed")
+            quit()
+        last = current
+        n += 1
+
+if n < 500000:
+    print(n, "< 1000000")
+    quit()
+
+print("ok")
